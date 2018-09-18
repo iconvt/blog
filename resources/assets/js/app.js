947d921526925aa52c7eccdd31a258a00f93aa0b
1053688433@qq.com <1053688433@qq.com>
2018-09-18 11:03:28 +0800

    Revert "第一次搭建vue 卡在路由了"
    This reverts commit baa2fa65a6114108969481d06ddcc274d97f518a.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,9 +5,9 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-// require('./bootstrap');
-//
-// window.Vue = require('vue');
+require('./bootstrap');
+
+window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,44 +15,8 @@
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue'));
-//
-// const app = new Vue({
-//     el: '#app'
-// });
-
-import Vue from 'vue';
-import routes from './route';
-// 导入element-ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-// import App from './components/ExampleComponent.vue';
-// import App from './App.vue';
-
-// 路由定义
-const NotFound = { template: '<p>Page not found</p>' }
-const Home = { template: '<p>home page</p>' }
-const About = { template: '<p>about page</p>' }
-
-Vue.use(ElementUI);
-// Vue.use(test);
-
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
-        el: '#app',
-        routes,
-        data:{
-            currentRoute:window.location.pathname
-        },
-        computed:{
-            ViewComponent (){
-                const matchingView = routes[this.currentRoute];
-                console.log(matchingView,1);
-                return matchingView?require('./pages/'+matchingView+'.vue'):require('./pages/404.vue');
-            }
-        },
-        render (h) {
-            return h(this.ViewComponent)
-        }
-    });
-window._app=app;
+    el: '#app'
+});
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-Route::get('/','IndexController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
